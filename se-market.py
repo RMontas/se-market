990ffe7356888af800df42e75aa1b823139f4b08
Ricardo Monteiro <rmonteiro@nvidia.com>
2021-06-08 21:32:51 +0200

    Switch to finding patterns within html code.

--- se-market.py
+++ se-market.py
@@ -1,19 +1,23 @@
-import pandas as pd
 import codecs
+import re
 
 def makeTable(html):
-    df = pd.read_html(html) # TRY BeautifulSoup
-    print(df)
+    buyPattern = "<tr class=\"center mini blue b670\" height=\"22\">"          # recuros - cred
+    sellPattern = "<tr class=\"center mini orange sep b670\" height=\"22\">"    # cred - recurso
+    matches = re.finditer(buyPattern, html)
+    matches_positions = [match.start() for match in matches]
+    print(matches_positions)
 
 def getMarketStats(html):
     f=codecs.open(html, 'r')
     return f.read()
 
+
 def main():
     htmlMarket = 'market.html'
     marketStats=getMarketStats(htmlMarket)
     print(marketStats)
-    makeTable(htmlMarket)
+    makeTable(marketStats)
 
 if __name__ == "__main__":
     main()
